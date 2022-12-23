e03d33bbda42093eba871dd621bc9a2c932eaf50
s51517765 <nakasha348@gmail.com>
2022-12-23 10:50:08 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,177 @@
+// https://akizukidenshi.com/catalog/g/gM-16731/
+// https://ja.aliexpress.com/item/1005002885902826.html
+
+#include <M5Stack.h> //0.3.9
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+#include <Adafruit_AHTX0.h> //2.0.3
+#include <WiFi.h>
+#include <WebServer.h>
+#include "auth.h"
+
+#define dateYpos 25
+#define dateXpos 34
+#define dateTextSize 3
+#define dateTextColor YELLOW
+#define timeXpos 60
+#define timeYpos 80
+#define timeTextSize 7 //(最大が7)
+#define timeTextColor GREEN
+#define tempYpos 160
+#define tempXpos 45
+#define tempTextColor WHITE
+#define tempTextSize 3
+#define secTimeOffset 13 // 環境に応じて設定
+
+Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
+Adafruit_AHTX0 aht;
+String sTemp;
+String sHum;
+int loopCount = 0;
+
+void wifiConnect()
+{
+  int tryCount = 0;
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password); // Wi-Fi接続
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    // Wi-Fi 接続待ち
+    delay(200);
+    Serial.printf(".");
+    tryCount += 1;
+    if (tryCount > 30)
+    {
+      Serial.println("\nwifi connect NG");
+      return;
+    };
+  }
+  Serial.println("\nwifi connect OK");
+}
+struct tm timeinfo;
+int weekday;
+void getTime()
+{
+  const long gmtOffset_sec = 3600 * 9 + secTimeOffset; // UTC + 9
+  const int daylightOffset_sec = 3600 * 0;             // Summer Timeなし
+  const char *ntpServer = "pool.ntp.org";
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+  String sweekday[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+  // 元の時刻を消去
+  M5.Lcd.setTextColor(BLACK);
+  M5.Lcd.setTextSize(dateTextSize);
+  M5.Lcd.setCursor(dateXpos, dateYpos);
+  M5.Lcd.print(&timeinfo, "%Y-%m-%d ");
+  M5.Lcd.print(sweekday[weekday]);
+  M5.Lcd.setCursor(timeXpos, timeYpos);
+  M5.Lcd.setTextSize(timeTextSize);
+  M5.Lcd.println(&timeinfo, "%H:%M");
+
+  // 新たな時刻を出力
+  if (!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  weekday = timeinfo.tm_wday; // 0 1 2 3 4 5 6
+
+  M5.Lcd.setTextColor(dateTextColor);
+  M5.Lcd.setTextSize(dateTextSize);
+  M5.Lcd.setCursor(dateXpos, dateYpos);
+  M5.Lcd.print(&timeinfo, "%Y-%m-%d ");
+  M5.Lcd.print(sweekday[weekday]);
+  M5.Lcd.setTextColor(timeTextColor);
+  M5.Lcd.setCursor(timeXpos, timeYpos);
+  M5.Lcd.setTextSize(timeTextSize);
+  M5.Lcd.println(&timeinfo, "%H:%M");
+
+  Serial.println(&timeinfo, "%Y-%m-%d %A %H:%M:%S");
+
+  if (loopCount == 0)
+  {
+    // 秒単位の時刻補正
+    int sec = timeinfo.tm_sec;
+    delay((60 - sec) * 1000);
+  }
+  return;
+}
+void getTemp()
+{
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp); // populate temp and humidity objects with fresh data
+
+  float t = temp.temperature;
+  float h = humidity.relative_humidity;
+  // LCD print
+  // 元の出力を消去
+  M5.Lcd.setTextSize(tempTextSize);
+  M5.Lcd.setCursor(tempXpos, tempYpos);
+  M5.Lcd.setTextColor(BLACK);
+  M5.Lcd.println(sTemp);
+  M5.Lcd.setCursor(tempXpos, tempYpos + tempTextSize * 10);
+  M5.Lcd.print(sHum);
+  // 新たな読み値を出力
+  sTemp = "Temp:" + String(t) + " C";
+  sHum = "Hum :" + String(h) + " RH";
+  M5.Lcd.setCursor(tempXpos, tempYpos);
+  M5.Lcd.setTextColor(tempTextColor);
+  M5.Lcd.println(sTemp);
+  M5.Lcd.setCursor(tempXpos, tempYpos + tempTextSize * 10);
+  M5.Lcd.print(sHum);
+
+  // Serial print
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degrees C");
+  Serial.print("Humidity: ");
+  Serial.print(humidity.relative_humidity);
+  Serial.println("% rH");
+}
+
+void setup()
+{
+  M5.begin(true, false, true);
+  M5.Power.begin();
+  M5.Lcd.setRotation(3);
+  Serial.begin(115200);
+  while (!Serial)
+  {
+  }
+  Serial.println("Serial Open!");
+
+  if (aht.begin())
+  {
+    Serial.println("Found AHT20");
+  }
+  else
+  {
+    Serial.println("Didn't find AHT20");
+  }
+
+  wifiConnect();
+}
+
+void loop()
+{
+  M5.update(); // ボタンを読み取る
+  if (M5.BtnB.wasReleased() || M5.BtnB.pressedFor(100, 200))
+  {
+    m5.Lcd.clear(BLACK);
+  }
+  if (loopCount % 10 == 0)
+  {
+    getTime();
+  }
+  loopCount += 1;
+  Serial.println(loopCount);
+
+  getTemp();
+
+  delay(1000);
+}
